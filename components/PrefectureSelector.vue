3e08bf4e5679cf3334237367f059ecf05ed7cc7b
Ledharni <LedharniWu@gmail.com>
2024-04-15 14:34:20 +0900

    PrefectureSelectorにドキュメントを追加

--- components/PrefectureSelector.vue
+++ components/PrefectureSelector.vue
@@ -20,21 +20,31 @@ import { ref, computed, defineEmits, watch } from 'vue'
 import { useFetch } from '#app'
 
 const emit = defineEmits(['updateSelectedPrefectures'])
+
+// 都道府県データをAPIから取得
 const { data, error } = useFetch('/api/getPrefectures')
+
+// 取得したデータをcomputedプロパティで管理
 const prefectures = computed(() => (data.value ? data.value.result : []))
+
+// 選択された都道府県を管理するref
 const selectedPrefectures = ref([])
 
+// 特定の都道府県が選択されているかどうかを判定する関数
 const isSelected = (prefCode) => {
   return selectedPrefectures.value.some(item => item.prefCode === prefCode)
 }
 
+// チェックボックスの変更をハンドルする関数
 const handleCheckboxChange = (prefecture) => {
   const index = selectedPrefectures.value.findIndex(
     item => item.prefCode === prefecture.prefCode
   )
   if (index > -1) {
+    // すでに選択されている場合は削除
     selectedPrefectures.value.splice(index, 1)
   } else {
+    // 選択されていない場合は追加
     selectedPrefectures.value.push({
 
       prefCode: prefecture.prefCode,
@@ -54,6 +64,7 @@ watch(selectedPrefectures, (newVal) => {
   text-align: center;
 }
 
+/* 都道府県リストのスタイリング */
 .prefecture-list {
   list-style: none;
   padding: 0;
@@ -63,6 +74,7 @@ watch(selectedPrefectures, (newVal) => {
   gap: 10px;
 }
 
+/* 各リストアイテムのスタイリング */
 .prefecture-list li {
   flex: 0 0 calc(20% - 10px);
   text-align: center;
